9182c3c1b58d0ad68d61652535530f07bcdde959
Thomas Hartwig <account@thomashartwig.com>
2024-06-15 02:04:18 +0200

    adaaaaqw

--- Package.swift
+++ Package.swift
@@ -43,5 +43,5 @@ let package = Package(
 
 #if os(Linux)
     package.dependencies.append(.package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"))
-    package.targets[0].dependencies.append("Crypto")
+    package.targets[0].dependencies.append(name: "Crypto", package: "swift-crypto")
 #endif
